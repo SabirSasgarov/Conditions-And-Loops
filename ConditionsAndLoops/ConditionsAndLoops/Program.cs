ca4b361b5d38a09a6dbd9ea0090758b01a748a04
SabirA <sabirsas@code.edu.az>
2026-01-30 00:10:09 +0400

    first app commit

---
+++ ConditionsAndLoops/ConditionsAndLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int a = 0;
+		int b = 100;
+		int sum = 0;
+
+		for (int i = a; i < b; i++)
+		{
+			if (i%2==0)
+				sum+=i;
+		}
+		Console.WriteLine("Sum: " + sum);
+	}
+}
