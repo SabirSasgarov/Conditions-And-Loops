7327d27f62cabf96ae8983c093478c7d61652cea
SabirA <sabirsas@code.edu.az>
2026-01-31 23:58:53 +0400

    Added some codes

--- ConditionsAndLoops/ConditionsAndLoops/Program.cs
+++ ConditionsAndLoops/ConditionsAndLoops/Program.cs
@@ -2,15 +2,33 @@
 {
 	private static void Main(string[] args)
 	{
-		int a = 0;
-		int b = 100;
-		int sum = 0;
+		//1
+		//int a = 0;
+		//int b = 100;
+		//int sum = 0;
 
-		for (int i = a; i < b; i++)
+		//for (int i = a; i < b; i++)
+		//{
+		//	if (i % 2 == 0)
+		//		sum += i;
+		//}
+		//Console.WriteLine("Sum: " + sum);
+
+
+		//2
+		int a = 13;
+		int count = 0;
+		for(int i=2; i<a/2; i++)
 		{
-			if (i%2==0)
-				sum+=i;
+			if (a % i == 0)
+				count++;
 		}
-		Console.WriteLine("Sum: " + sum);
+		if(count>0)
+			Console.WriteLine("Murekkeebdir");
+		else
+			Console.WriteLine("Sadedir");
+
+
+
 	}
 }
